488e5d0c8455b5ca1b015fa98a88278892ae1828
ginogen <ginogentile@live.com>
2019-05-01 23:16:54 -0300

    Updates

---
+++ precache-manifest.76e498f4e57c17aa1856d7103a8f908a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33599c0713f528f302294a3c0e400cfa",
+    "url": "/react-portfolio/portfolio/index.html"
+  },
+  {
+    "revision": "52e4d011f3d482dca9ce",
+    "url": "/react-portfolio/portfolio/static/css/main.4abafa4a.chunk.css"
+  },
+  {
+    "revision": "d033208bdf8aab099668",
+    "url": "/react-portfolio/portfolio/static/js/2.080ff310.chunk.js"
+  },
+  {
+    "revision": "52e4d011f3d482dca9ce",
+    "url": "/react-portfolio/portfolio/static/js/main.5ab882a5.chunk.js"
+  },
+  {
+    "revision": "35804bc47f268758be3b",
+    "url": "/react-portfolio/portfolio/static/js/runtime~main.72eba121.js"
+  }
+]);
